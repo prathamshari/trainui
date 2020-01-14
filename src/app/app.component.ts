78c7b62e0b3388eeec8974aa893fd6a4ed146c95
sharvari@gmail.com <sharu@gmail.com>
2020-01-14 11:42:46 +0530

    angular added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+  constructor(private service:AuthService,
+    private router:Router) 
+    { }
+
+    Logout()
+    {
+      this.service.LogOut();
+      this.router.navigate(['login']);
+    }
 }
